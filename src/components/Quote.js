4303b2886f2b81703225ec190d17d5d4fb496a3e
Adriel <adriel.fsantos@outlook.com>
2021-11-20 20:52:13 -0300

    Adicicomponente Quote

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Quote.css";
+
+const Quote = (props) => {
+  return (
+    <>
+      <p id="text">Quote</p>
+      <p id="author">- Author</p>
+    </>
+  );
+};
+
+export default Quote;
